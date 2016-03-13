91b5d59f9710960fea49c4b9d4c858ce3100ba2d
Braun Patrik <bra.patrik@gmail.com>
2016-03-13 21:31:49 +0100

    updating package

--- frontend/app/login/login.service.ts
+++ frontend/app/login/login.service.ts
@@ -3,7 +3,6 @@
 import {Injectable} from 'angular2/core';
 import {NetworkService} from "../model/network.service";
 import {OnInit} from "angular2/core";
-import {MessageTypes} from "../../../common/MessageTypes";
 import {LoginCredential} from "../../../common/entities/LoginCredential";
 import {User} from "../../../common/entities/User";
 import {Message} from "../../../common/entities/Message";
