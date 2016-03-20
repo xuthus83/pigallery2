0d9690f03205b6caf4220a3c40e7f4fca4c66381
Braun Patrik <bra.patrik@gmail.com>
2016-03-20 17:19:04 +0100

    packages refactoring

--- frontend/browser.d.ts
+++ frontend/browser.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="../typings/browser.d.ts"/>
 /// <reference path="../common/common-classes.d.ts" />
-/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
-/// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts" />
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" /> 
 /// <reference path="../node_modules/tslint/lib/tslint.d.ts" />
 /// <reference path="../node_modules/zone.js/dist/zone.js.d.ts"/>
 
