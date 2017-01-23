935b723bdb91b96e3b942b07f0865513174d0b30
Braun Patrik <braun.patrik@aut.bme.hu>
2017-01-23 21:29:07 +0100

    positioning small map

--- frontend/app/gallery/map/map.gallery.component.ts
+++ frontend/app/gallery/map/map.gallery.component.ts
@@ -10,18 +10,24 @@ export class GalleryMapComponent implements OnChanges {
 
     @Input() photos: Array<PhotoDTO>;
 
-    mapPhotos: Array<{latitude, longitude, iconUrl}> = [];
-
+    mapPhotos: Array<{latitude: string, longitude: string, iconUrl}> = [];
+    mapCenter = {latitude: "0", longitude: "0"};
 
     //TODO: fix zooming
     ngOnChanges() {
-        this.mapPhotos = this.photos.filter(p => p.metadata.positionData.GPSData).map(p => {
+        this.mapPhotos = this.photos.filter(p => {
+            return p.metadata && p.metadata.positionData && p.metadata.positionData.GPSData;
+        }).map(p => {
             return {
                 latitude: p.metadata.positionData.GPSData.latitude,
                 longitude: p.metadata.positionData.GPSData.longitude,
                 iconUrl: Utils.concatUrls("/api/gallery/content/", p.directory.path, p.directory.name, p.name, "icon")
             };
         });
+
+        if (this.mapPhotos.length > 0) {
+            this.mapCenter = this.mapPhotos[0];
+        }
 
 
     }
