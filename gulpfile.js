dd1a86819a0398d7438d228ab297c957a28a95d6
Braun Patrik <braun.patrik@aut.bme.hu>
2017-06-02 23:48:46 +0200

    improving release scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,34 @@
 var ts = require('gulp-typescript');
-var del = require('del');
 var gulp = require('gulp');
-var merge = require('merge2');
 var zip = require('gulp-zip');
 var runSequence = require('run-sequence');
 
-
-
-gulp.task('clean', function () {
-    return del('release/');
-});
-
-gulp.task("copy-files", function () {
-    return gulp
-        .src(['package.json',
-            ".npmignore",
-            "frontend/**/*.+(png|ejs|html|css)",
-            "frontend/systemjs*"], {base: "."})
-        .pipe(gulp.dest("release"));
-});
-
+var tsProject = ts.createProject('tsconfig.json');
 gulp.task('compile-release', function () {
+    return gulp.src([
+        "frontend/**/*.ts",
+        "common/**/*.ts",
+        "backend/**/*.ts"], {base: "."})
+        .pipe(tsProject())
+        .js
+        .pipe(gulp.dest("."))
 
-    return merge(
-        ['frontend',
-            'common',
-            'backend'].map(function (dir) {
-            var tsProject = ts.createProject('tsconfig.json');
-            return gulp.src(dir + "/**/*.ts")
-                .pipe(tsProject())
-                .js
-                .pipe(gulp.dest("release/" + dir))
-        })
-    );
 });
 gulp.task('zip-release', function () {
-    return gulp.src('release/**/*')
+    return gulp.src(['package.json',
+        "README.md",
+        "LICENSE",
+        "frontend/**/*.js",
+        "common/**/*.js",
+        "backend/**/*.js",
+        "frontend/**/*.+(png|ejs|html|css)",
+        "frontend/systemjs*"], {base: "."})
         .pipe(zip('pigallery2.zip'))
-        .pipe(gulp.dest('release'))
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('build-release', function (done) {
-    runSequence('clean', 'copy-files', 'compile-release', 'zip-release', function () {
+    runSequence('compile-release', 'zip-release', function () {
         console.log('Run something else');
         done();
     });
