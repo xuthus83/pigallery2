6489fe3930f53a7cc0216eec3fb86f93d6bf63b2
Patrik J. Braun <bra.patrik@gmail.com>
2019-02-04 18:00:30 -0500

    fixing interface implementation error

--- backend/model/memory/PersonManager.ts
+++ backend/model/memory/PersonManager.ts
@@ -1,6 +1,11 @@
 import {IPersonManager} from '../interfaces/IPersonManager';
+import {MediaDTO} from '../../../common/entities/MediaDTO';
 
 export class IndexingTaskManager implements IPersonManager {
+  keywordsToPerson(media: MediaDTO[]): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
   get(name: string): Promise<any> {
     throw new Error('not supported by memory DB');
   }
