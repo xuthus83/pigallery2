6d44c22df13b8f9416715e86e160707d0c3f973a
Patrik J. Braun <bra.patrik@gmail.com>
2019-07-21 17:26:23 +0200

    fixing install error

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,16 +1,10 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {ApplicationRef, enableProdMode} from '@angular/core';
+import {enableProdMode} from '@angular/core';
 import {environment} from './environments/environment';
 import {AppModule} from './app/app.module';
-import {enableDebugTools} from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
-} 
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(moduleRef => {
-  const applicationRef = moduleRef.injector.get(ApplicationRef);
-  const componentRef = applicationRef.components[0];
-  // allows to run `ng.profiler.timeChangeDetection();`
-  enableDebugTools(componentRef);
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
