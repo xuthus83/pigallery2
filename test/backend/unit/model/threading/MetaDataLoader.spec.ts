ffa7840626966ad7331f029e1745088dc8fceacd
Patrik J. Braun <bra.patrik@gmail.com>
2020-09-13 14:43:03 +0200

    Adding face testing for png #99

--- test/backend/unit/model/threading/MetaDataLoader.spec.ts
+++ test/backend/unit/model/threading/MetaDataLoader.spec.ts
@@ -8,14 +8,8 @@ describe('MetadataLoader', () => {
   it('should load png', async () => {
     const data = await MetadataLoader.loadPhotoMetadata(path.join(__dirname, '/../../../assets/test_png.png'));
     delete data.creationDate; // creation time for png not supported
-    expect(Utils.clone(data)).to.be.deep.equal(Utils.clone({
-      fileSize: 2155,
-      orientation: 1,
-      size: {
-        height: 26,
-        width: 26
-      }
-    }));
+    const expected = require(path.join(__dirname, '/../../../assets/test_png.json'));
+    expect(Utils.clone(data)).to.be.deep.equal(expected);
   });
 
   it('should load jpg', async () => {
