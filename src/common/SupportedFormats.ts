e345da5e05e6b8fd5978ba4c8b2a5f7c02258931
Patrik J. Braun <bra.patrik@gmail.com>
2022-11-23 20:45:31 +0100

    bumping sharp to 0.32.2 #295

--- src/common/SupportedFormats.ts
+++ src/common/SupportedFormats.ts
@@ -1,5 +1,5 @@
 export const SupportedFormats = {
-  Photos: ['gif', 'jpeg', 'jpg', 'jpe', 'png', 'webp', 'svg'],
+  Photos: ['gif', 'jpeg', 'jpg', 'jpe', 'png', 'webp', 'svg', 'heic'],
   // Browser supported video formats
   // Read more:  https://www.w3schools.com/html/html5_video.asp
   Videos: ['mp4', 'webm', 'ogv', 'ogg'],
