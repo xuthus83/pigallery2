eea77a5bea583261bcbeeae3b7ab4d9fe055f4a6
Patrik J. Braun <bra.patrik@gmail.com>
2022-12-04 21:52:49 +0100

    Adding more indices to fields and improving SQL queries #437

---
+++ src/backend/model/database/sql/enitites/PersonJunctionTable.ts
@@ -0,0 +1,28 @@
+import {Entity, Index, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
+import {PersonEntry} from './PersonEntry';
+import {MediaEntity} from './MediaEntity';
+
+
+/**
+ * This is a junction table between media and persons
+ */
+@Entity()
+export class PersonJunctionTable {
+  @Index()
+  @PrimaryGeneratedColumn({unsigned: true})
+  id: number;
+
+  @Index()
+  @ManyToOne((type) => MediaEntity, (media) => media.metadata.faces, {
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  media: MediaEntity;
+
+  @Index()
+  @ManyToOne((type) => PersonEntry, (person) => person.faces, {
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  person: PersonEntry;
+}
