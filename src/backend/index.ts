203fc798ac2e21ff915d4209e46fcc38409802cc
Patrik J. Braun <bra.patrik@gmail.com>
2023-01-13 00:32:58 +0100

    Add build time diagnostics to docker to test failing sharp #588 #295

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,9 +1,21 @@
 import * as cluster from 'cluster';
 import {Server} from './server';
 import {Worker} from './model/threading/Worker';
+import {ConfigDiagnostics} from './model/diagnostics/ConfigDiagnostics';
 
-if ((cluster as any).isMaster) {
-  new Server();
+
+if ((process.argv || []).includes('--run-diagnostics')) {
+  console.log('Running diagnostics and exiting.');
+  ConfigDiagnostics.runDiagnostics().catch((e) => {
+    console.error(e);
+    process.exit(1);
+  }).then(() => {
+    process.exit(0);
+  });
 } else {
-  Worker.process();
+  if ((cluster as any).isMaster) {
+    new Server();
+  } else {
+    Worker.process();
+  }
 }
