e7c0906cb266052e1ab758c77269e0e0970a1772
Nick Kocharhook <nick@kocharhook.com>
2023-11-27 22:47:24 +0000

    [#760] Add image titles and captions to the info panel.

--- src/backend/model/fileaccess/MetadataLoader.ts
+++ src/backend/model/fileaccess/MetadataLoader.ts
@@ -342,6 +342,9 @@ export class MetadataLoader {
               .replace(/\0/g, '')
               .trim();
           }
+          if (iptcData.object_name) {
+            metadata.title = iptcData.object_name.replace(/\0/g, '').trim();
+          }
           if (iptcData.caption) {
             metadata.caption = iptcData.caption.replace(/\0/g, '').trim();
           }
