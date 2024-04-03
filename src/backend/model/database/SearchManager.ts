7f68c827d89490165ec47a5789e349f5c8a6b509
gras <chrisc.gigamail@gmail.com>
2024-04-03 21:16:59 +0200

    This reverts commit 84928e6371988c302d5fd13504d324df00195c1d.

--- src/backend/model/database/SearchManager.ts
+++ src/backend/model/database/SearchManager.ts
@@ -779,7 +779,7 @@ export class SearchManager {
                 break;
 
               case DatePatternFrequency.months_ago:
-                to.setTime(Utils.addMonthToDate(to, -1 * tq.agoNumber).getTime());
+                to.setUTCMonth(to.getUTCMonth() - tq.agoNumber);
                 break;
 
               case DatePatternFrequency.years_ago:
