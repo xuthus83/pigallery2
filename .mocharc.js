8dad5825176550dbfcfa94e29b0d34b277ffe8fd
Chris <chrisc.gigamail@gmail.com>
2024-10-27 00:08:37 +0200

    improved local testing possibilities

---
+++ .mocharc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    timeout: 20000, // Set timeout to 20 seconds (good for slow test setups)
+    require: 'test/setup-local.js',
+    recursive: true, // Run tests in subdirectories
+  };
